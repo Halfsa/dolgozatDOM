72d8e26cd064a5f3248499ebc4a0e68be22b023d
Petrik Diak <diak@petrik.hu>
2023-11-06 15:12:14 +0100

    gsgvd

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,17 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import quotes from '../'
+const k =  Array.from('../quotes.json');
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function OsszesIdezet() {
+  var egyes = k.sort();
+}
+function init() {
+  const feladat1 = document.getElementById('feladat1');
+  feladat1!.addEventListener('click',() =>{
+    OsszesIdezet();
+  });
+  for (let i = 0; i < k.length; i++) {
+    console.log(k[i]);
+    
+  }
+}
+document.addEventListener('DOMContentLoaded',init);
